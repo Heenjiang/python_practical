207f7e225b8e2db8693722614b5958cebf6c7159
Lucas <1207881522@qq.com>
2020-03-08 00:40:39 +0300

    orm comments and test case

--- www/orm.py
+++ www/orm.py
@@ -64,26 +64,27 @@ async def execute(sql, args, autocommit=True):
                 await conn.rollback()
             raise
         return affected
-#
+#根据参数的个数，创建预定义的参数列表:(?, ?, ?, ?)
 def create_args_string(num):
     L = []
     for n in range(num):
         L.append('?')
     return ', '.join(L)
 
+#model类属性的基本类型
 class Field(object):
-
+    #初始化方法，
     def __init__(self, name, column_type, primary_key, default):
         self.name = name
         self.column_type = column_type
         self.primary_key = primary_key
         self.default = default
-
+    #重写__str__方法
     def __str__(self):
         return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
-
+#继承自Field基本类型的string
 class StringField(Field):
-
+    #初始化，并且调用父类的__init__方法， varchar: 不定长字符串，注意不能超过定义的长度，否则超过的部分被自动截断
     def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
         super().__init__(name, ddl, primary_key, default)
 
@@ -100,6 +101,7 @@ class IntegerField(Field):
 class FloatField(Field):
 
     def __init__(self, name=None, primary_key=False, default=0.0):
+        #real:是sql的一种数据类型，一个real类型的数据占4个字节，它可以描述7个精度
         super().__init__(name, 'real', primary_key, default)
 
 class TextField(Field):
@@ -107,22 +109,28 @@ class TextField(Field):
     def __init__(self, name=None, default=None):
         super().__init__(name, 'text', False, default)
 
+#python用来创建并实现ORM的魔术类
 class ModelMetaclass(type):
-
+    #这个方法用于具体创建
     def __new__(cls, name, bases, attrs):
+        #如果调用这个方法的实例是model的直接类型，就返回
         if name=='Model':
             return type.__new__(cls, name, bases, attrs)
+        #获取Model子类型实列的所对应的数据库表的名称，如果没有就默认为该类型的类名
         tableName = attrs.get('__table__', None) or name
         logging.info('found model: %s (table: %s)' % (name, tableName))
+        #Model子类类属性的对应关系
         mappings = dict()
+        #除开主键外的其他类属性对应关系
         fields = []
         primaryKey = None
         for k, v in attrs.items():
             if isinstance(v, Field):
                 logging.info('  found mapping: %s ==> %s' % (k, v))
                 mappings[k] = v
+                # 找到主键:
                 if v.primary_key:
-                    # 找到主键:
+                    # 如果找到主键之后，又出现主键
                     if primaryKey:
                         raise Exception('Duplicate primary key for field: %s' % k)
                     primaryKey = k
@@ -130,8 +138,10 @@ class ModelMetaclass(type):
                     fields.append(k)
         if not primaryKey:
             raise Exception('Primary key not found.')
+        #将已经添加到mapping的属性从attrs中删除
         for k in mappings.keys():
             attrs.pop(k)
+        #map(function(), Iterable)是python的全局函数，用于对Iterable对象中的每个元素应用function()，返回一个Iterator 
         escaped_fields = list(map(lambda f: '`%s`' % f, fields))
         attrs['__mappings__'] = mappings # 保存属性和列的映射关系
         attrs['__table__'] = tableName
@@ -142,7 +152,10 @@ class ModelMetaclass(type):
         attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
         attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
         return type.__new__(cls, name, bases, attrs)
-
+#所有model子类的父类，继承了dict和ModelMetaclass.
+#这样的好处是所有的model的子类隐形继承了metaclass，当创建实例时，python解释器会检查当前类的定义和父类的定义有没有metacalss，
+#如果有就会直接调用metaclass类的__new()__方法创建对象
+#又由于Model也继承了dict，所以堆属性的访问，可以使用a.b或者a[b]
 class Model(dict, metaclass=ModelMetaclass):
 
     def __init__(self, **kw):
@@ -156,24 +169,29 @@ class Model(dict, metaclass=ModelMetaclass):
 
     def __setattr__(self, key, value):
         self[key] = value
-
+    #获取实例属性的值
     def getValue(self, key):
         return getattr(self, key, None)
 
     def getValueOrDefault(self, key):
         value = getattr(self, key, None)
+        #如果实例属性值为None，就获取对应的类属性的默认值
         if value is None:
             field = self.__mappings__[key]
             if field.default is not None:
+                #如果类属性的是一个函数就调用
                 value = field.default() if callable(field.default) else field.default
                 logging.debug('using default value for %s: %s' % (key, str(value)))
+                #将类属性的默认值赋给实例
                 setattr(self, key, value)
         return value
-
+    # @classmethod是用来修饰类方法的，用该标识符修饰的方法不需要传入self对象，所以可以直接类名.方法名调用
     @classmethod
     async def findAll(cls, where=None, args=None, **kw):
         ' find objects by where clause. '
+        #这个方法一般是由类名直接调用的，例如：User.findAll(args...)
         sql = [cls.__select__]
+        #根据参数添加sql的子句
         if where:
             sql.append('where')
             sql.append(where)
@@ -194,7 +212,9 @@ class Model(dict, metaclass=ModelMetaclass):
                 args.extend(limit)
             else:
                 raise ValueError('Invalid limit value: %s' % str(limit))
+        #由于定义所有__***sql__的时候，都是list，所以最后拼接的时候直接每个子句空格隔开就行了
         rs = await select(' '.join(sql), args)
+        #cla(**r)创建实例，最后返回的是一个实例列表
         return [cls(**r) for r in rs]
 
     @classmethod
---
+++ www/test.py
@@ -0,0 +1,2 @@
+fields = ['hello', 'Nicole', 'this', 'is', 'Hoen']
+print(list(map(lambda f: '`%s`' % f, fields)))
