4a4f35fcbdf9606463a806b8db33fc724f46e620
Lucas <1207881522@qq.com>
2020-03-11 15:27:07 +0300

    sign In, register, cookie impletation

--- www/app.py
+++ www/app.py
@@ -14,8 +14,12 @@ from datetime import datetime
 from aiohttp import web
 from jinja2 import Environment, FileSystemLoader
 
+from config import configs
+
 import orm
 from coroweb import add_routes, add_static
+
+from handlers import cookie2user, COOKIE_NAME
 #加载jinjia 模板模块
 def init_jinja2(app, **kw):
     logging.info('init jinja2...')
@@ -49,6 +53,22 @@ async def logger_factory(app, handler):
         # await asyncio.sleep(0.3)
         return (await handler(request))
     return logger
+
+async def auth_factory(app, handler):
+    @asyncio.coroutine
+    def auth(request):
+        logging.info('check user: %s %s' % (request.method, request.path))
+        request.__user__ = None
+        cookie_str = request.cookies.get(COOKIE_NAME)
+        if cookie_str:
+            user = yield from cookie2user(cookie_str)
+            if user:
+                logging.info('set current user: %s' % user.email)
+                request.__user__ = user
+        if request.path.startswith('/manage/') and (request.__user__ is None or not request.__user__.admin):
+            return web.HTTPFound('/signin')
+        return (yield from handler(request))
+    return auth
 
 async def data_factory(app, handler):
     #解析request请求的数据
